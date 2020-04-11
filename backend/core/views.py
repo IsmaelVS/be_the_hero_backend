e6ad16df7d13491ef0fd6233fc53935853301f90
Ismael Ventura <ismael.ventura@ies2.com.br>
2020-04-11 16:12:45 -0300

    Adicionando endpoint para criação de ongs.

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,14 +1,23 @@
+from django.http import JsonResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from json import dumps, loads
+from core.models import Ongs
 
 
 @csrf_exempt
-def index(request):
+def ongs(request):
     if request.method == 'POST':
-        return HttpResponse("POST")
-    if request.method == 'PUT':
-        return HttpResponse("PUT")
-    if request.method == 'DELETE':
-        return HttpResponse("DELETE")
-    return HttpResponse("Hello world!")
+        body = loads(request.body)
+
+        ong = Ongs(
+            nome=body['nome'],
+            email=body['email'],
+            whatsapp=body['whatsapp'],
+            cidade=body['cidade'],
+            uf=body['uf']
+        )
+        ong.save()
+
+        return JsonResponse({'id': ong.id}, safe=False)
