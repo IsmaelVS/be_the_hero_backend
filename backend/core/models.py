023f8edd09ddd5897651365cbe5461e8a98137af
Ismael Ventura <ismael.ventura@ies2.com.br>
2020-04-11 15:48:44 -0300

    Criação dos models.

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Ongs(models.Model):
+    nome = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
+    whatsapp = models.CharField(max_length=15)
+    cidade = models.CharField(max_length=15)
+    uf = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.nome
+
+class Incidents(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=50)
+    value = models.FloatField()
+    ong = models.ForeignKey(Ongs, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
