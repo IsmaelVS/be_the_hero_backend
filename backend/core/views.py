dd9dca2444e54096510343143745f83664a4e0e7
Ismael Ventura <ismael.ventura@ies2.com.br>
2020-04-11 16:20:19 -0300

    Endpoint para listar incidents de uma ong específica.

--- backend/core/views.py
+++ backend/core/views.py
@@ -89,3 +89,21 @@ def incident(request, id):
             return JsonResponse({'DELETE': id}, safe=False, status=204)
 
         return JsonResponse({'Error': 'Operation not permitted'}, safe=False, status=401)
+
+
+@csrf_exempt
+def list_incidents_from_an_ong(request):
+    if request.method == 'GET':
+        ong_id = request.headers['Authorization']
+        all_incidents = Incidents.objects.filter(ong_id=ong_id)
+        incidents = []
+        for incident in all_incidents:
+            incidents.append({
+                'id': incident.id,
+                'title': incident.title,
+                'description': incident.description,
+                'value': incident.value,
+                'ong': incident.ong_id,
+            })
+
+        return JsonResponse(incidents, safe=False)
