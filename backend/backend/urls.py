e1f6e22da0dc0b7eb7204b6569cd39caf38a84c9
Ismael Ventura <ismael.ventura@ies2.com.br>
2020-04-11 15:48:30 -0300

    Adicionando a app no settings e adicionando as urls do projeto.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,14 @@
-from django.conf.urls import url
-from django.urls import include
+from django.urls import path
 from django.contrib import admin
 
+from core import views
+
+
 urlpatterns = [
-    url('', include('core.urls')),
-    url('admin/', admin.site.urls),
+    path('ongs/', views.ongs),
+    path('incidents/', views.incidents),
+    path('incidents/<int:id>/', views.incident),
+    path('incidents/ong/', views.list_incidents_from_an_ong),
+    path('login/', views.login),
+    path('admin/', admin.site.urls),
 ]
