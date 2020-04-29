bc2f6a4b8386e264183abf22b7f39353d68f7021
Ismael Ventura <ismael.ventura@ies2.com.br>
2020-04-29 18:03:40 -0300

    Teste unitários da rota de criação de incidents.

--- backend/core/tests/tests_views.py
+++ backend/core/tests/tests_views.py
@@ -1,14 +1,16 @@
+"""Tests for views.py endpoints."""
 from json import dumps, loads
 
 from django.test import Client, TestCase
 
-from ..models import Incidents
-from ..models import ONGs
+from ..models import Incidents, ONGs
 
 
 class TestsONGs(TestCase):
+    """ONG testing class."""
 
     def setUp(self):
+        """Prepare environment for tests."""
         self.client = Client(enforce_csrf_checks=False)
 
         data_ong = dumps({
@@ -23,7 +25,7 @@ class TestsONGs(TestCase):
                          content_type="application/json", data=data_ong)
 
     def test_created_ong(self):
-
+        """Test creating new ONG."""
         data = dumps({
             "name": "ONG2",
             "email": "hihi@ong.com",
@@ -43,7 +45,7 @@ class TestsONGs(TestCase):
         self.assertEqual(ongs, 2)
 
     def test_data_with_incomplete(self):
-
+        """New ONG with data incomplete."""
         data = dumps({
             "name": "ONG3",
             "email": "hihi@ong.com",
@@ -60,3 +62,133 @@ class TestsONGs(TestCase):
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response_data['Fail'], 'Data is missing.')
         self.assertEqual(ongs, 1)
+
+
+class TestsIncidents(TestCase):
+    """Incident testing class."""
+
+    def setUp(self):
+        """Prepare environment for tests."""
+        self.client = Client(enforce_csrf_checks=False)
+
+        data_ong = dumps({
+            "name": "ONG",
+            "email": "hihi@ong.com",
+            "whatsapp": "1111111111",
+            "city": "Capão Redondo",
+            "uf": "SP"
+        })
+
+        self.client.post('/ongs/',
+                         content_type="application/json", data=data_ong)
+
+        ong = ONGs.objects.get(id=1)
+
+        data_inc = dumps({
+            "title": "Caso - teste",
+            "description": "hihihihihihihihihihihihihihi",
+            "value": "1100",
+            "ong": ong.id,
+        })
+
+        header = {'HTTP_Authorization': ong.id}
+
+        self.client.post('/incidents/',
+                         content_type="application/json",
+                         data=data_inc, **header
+                         )
+
+    def test_created_incident(self):
+        """Test creating new incident."""
+        ong = ONGs.objects.get(id=1)
+
+        data = dumps({
+            "title": "Caso - teste",
+            "description": "hihihihihihihihihihihihihihi",
+            "value": "1100",
+            "ong": ong.id,
+        })
+
+        header = {'HTTP_Authorization': '1'}
+
+        response = self.client.post('/incidents/',
+                                    content_type="application/json",
+                                    data=data, **header
+                                    )
+
+        incidents = Incidents.objects.count()
+        response_data = loads(response.content)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response_data['id'], 2)
+        self.assertEqual(incidents, 2)
+
+    def test_data_with_incomplete(self):
+        """New incident with data incomplete."""
+        ong = ONGs.objects.get(id=1)
+
+        data = dumps({
+            "description": "hihihihihihihihihihihihihihi",
+            "value": "1100",
+            "ong": ong.id,
+        })
+
+        header = {'HTTP_Authorization': '1'}
+
+        response = self.client.post('/incidents/',
+                                    content_type="application/json",
+                                    data=data, **header
+                                    )
+
+        incidents = Incidents.objects.count()
+        response_data = loads(response.content)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response_data['Fail'], 'Data is missing.')
+        self.assertEqual(incidents, 1)
+
+    def test_without_header(self):
+        """New incident without header."""
+        ong = ONGs.objects.get(id=1)
+
+        data = dumps({
+            "title": "Caso - teste2",
+            "description": "hihihihihihihihihihihihihihi",
+            "value": "1100",
+            "ong": ong.id,
+        })
+
+        response = self.client.post('/incidents/',
+                                    content_type="application/json",
+                                    data=data
+                                    )
+
+        incidents = Incidents.objects.count()
+        response_data = loads(response.content)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response_data['Fail'], 'Data is missing.')
+        self.assertEqual(incidents, 1)
+
+    def testing_with_ong_invalid(self):
+        """New incident with wrong header."""
+        data = dumps({
+            "title": "Caso - teste3",
+            "description": "hihihihihihihihihihihihihihi",
+            "value": "1100",
+            "ong": 500,
+        })
+
+        header = {'HTTP_Authorization': '500'}
+
+        response = self.client.post('/incidents/',
+                                    content_type="application/json",
+                                    data=data, **header
+                                    )
+
+        incidents = Incidents.objects.count()
+        response_data = loads(response.content)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response_data['Error'], 'Ong invalid.')
+        self.assertEqual(incidents, 1)
