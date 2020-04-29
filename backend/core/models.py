95e498846f4147c86f29ed63434557fcf580a693
Ismael Ventura <ismael.ventura@ies2.com.br>
2020-04-28 23:52:35 -0300

    Corrigindo nomenclatura de campos nos models.

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,21 +1,22 @@
 from django.db import models
 
 
-class Ongs(models.Model):
-    nome = models.CharField(max_length=30)
+class ONGs(models.Model):
+    name = models.CharField(max_length=30)
     email = models.CharField(max_length=50)
     whatsapp = models.CharField(max_length=15)
-    cidade = models.CharField(max_length=15)
+    city = models.CharField(max_length=15)
     uf = models.CharField(max_length=2)
 
     def __str__(self):
-        return self.nome
+        return self.name
+
 
 class Incidents(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=50)
     value = models.FloatField()
-    ong = models.ForeignKey(Ongs, on_delete=models.CASCADE)
+    ong = models.ForeignKey(ONGs, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
